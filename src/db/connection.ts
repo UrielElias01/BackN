3a503ff3a2b2ab56b3f0522d1197d45bb3baf40b
uriel <aelias@lcamx.com>
2024-05-15 17:52:15 -0600

    commit

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize('blw1ovrym0bkhxcv6hpp', 'u4knkkd9u1ct2n0f', 'HlLoacRs0hvy7n6w8G4Q', {
-    host: 'blw1ovrym0bkhxcv6hpp-mysql.services.clever-cloud.com',
+const sequelize = new Sequelize('via_flex', 'root', '123456', {
+    host: 'localhost',
     dialect: 'mysql',   
 });
 
