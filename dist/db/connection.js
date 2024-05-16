4e9f417c701b3ca490764f3652d8b6849a8cd245
uriel <aelias@lcamx.com>
2024-05-15 20:28:01 -0600

    commit4

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -4,9 +4,9 @@ const sequelize_1 = require("sequelize");
 const config_1 = require("../models/config");
 // Convertir DB_PORT a número si es una cadena válida
 const port = typeof config_1.DB_PORT === 'string' ? parseInt(config_1.DB_PORT) : undefined;
-const sequelize = new sequelize_1.Sequelize(config_1.DB_NAME, config_1.DB_USER, config_1.DB_PASSWORD, {
-    host: config_1.DB_HOST,
+const sequelize = new sequelize_1.Sequelize('railway', 'root', 'zqUXZYwTmcnAfaAjigJHRylDAllOScDZ', {
+    host: 'roundhouse.proxy.rlwy.net',
     dialect: 'mysql',
-    port: port // Asignar el puerto convertido a número
+    port: 43713 // Asignar el puerto convertido a número
 });
 exports.default = sequelize;
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,10 +4,10 @@ import { DB_NAME, DB_PASSWORD, DB_HOST, DB_USER, DB_PORT } from "../models/confi
 // Convertir DB_PORT a número si es una cadena válida
 const port: number | undefined = typeof DB_PORT === 'string' ? parseInt(DB_PORT) : undefined;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
+const sequelize = new Sequelize('railway', 'root', 'zqUXZYwTmcnAfaAjigJHRylDAllOScDZ', {
+    host: 'roundhouse.proxy.rlwy.net',
     dialect: 'mysql',
-    port: port // Asignar el puerto convertido a número
+    port: 43713// Asignar el puerto convertido a número
 });
 
 export default sequelize;
