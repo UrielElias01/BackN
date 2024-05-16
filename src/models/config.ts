045991acb466a5c5c03e8f1259bb387ef80c64f6
uriel <aelias@lcamx.com>
2024-05-15 19:38:40 -0600

    commit4

--- src/models/config.ts
+++ src/models/config.ts
@@ -4,4 +4,4 @@ export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '123456'
 export const DB_NAME = process.env.DB_NAME || 'via_flex'
-export const DB_PORT = process.env.BD_PORT || 3306
+export const DB_PORT = process.env.DD_PORT || 3306
