66d9c355fd01d54f3c5adc92fcbf81923693231c
uriel <aelias@lcamx.com>
2024-05-15 19:22:51 -0600

    commit4

---
+++ dist/db/connection.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const config_1 = require("../models/config");
+const sequelize = new sequelize_1.Sequelize(config_1.DB_NAME, config_1.DB_USER, config_1.DB_PASSWORD, {
+    host: config_1.DB_HOST,
+    dialect: 'mysql',
+});
+exports.default = sequelize;
